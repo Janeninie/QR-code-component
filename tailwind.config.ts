6300bb91d4081287f135109aa58038c3f7c97ebb
Jane <apiwatsupaphol@gmail.com>
2024-12-15 02:29:17 +0700

    feat: added qr

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,14 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          300: "hsl(212, 45%, 89%)",
+          500: "hsl(216, 15%, 48%)",
+          900: "hsl(218, 44%, 22%)",
+        },
+      },
+      boxShadow: {
+        custom: "0 0 40px 0 rgba(0, 0, 0, 0.1)",
       },
     },
   },
